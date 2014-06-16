3b09c46cd952d7ff8db3294f947c615ec7a53c13
Patrick Beeson <patrickbeeson@gmail.com>
2014-06-16 15:42:24 -0400

    Add production options to config, remove pyc files, update gitignore, add fabfile.

--- config.py
+++ config.py
@@ -1,21 +1,27 @@
 SECRET_KEY = '\x03V\x96\xb6{#\x9f\x14K\xef{\x86Z\x14\xafZK/-\x84\xb0O\xcd\x13'
 
+# Testing
 STRIPE_SECRET_KEY = 'sk_test_HsuAieq5wo8ZSAm3lEushARi'
-
 STRIPE_PUBLIC_KEY = 'pk_test_7MmO7bkJvlkV0TfD1YfFcnVf'
 
-SQLALCHEMY_DATABASE_URI = 'sqlite:///sharethesunshine.db'
+# Live
+
+# Development
+#SQLALCHEMY_DATABASE_URI = 'sqlite:///sharethesunshine.db'
+
+# Production
+SQLALCHEMY_DATABASE_URI = 'postgresql://sharethesunshine:ClearsTheClouds@localhost:5432/sharethesunshine'
 
 ADMINS = ['pbeeson@thevariable.com']
 
 MAIL_SERVER = 'smtp.webfaction.com'
 
-MAIL_USERNAME = 'patrickbeeson_mail'
+MAIL_USERNAME = 'sharethesunshine'
 
-MAIL_PASSWORD = '6ElevenBicycleC0'
+MAIL_PASSWORD = 'V@r1able'
 
 MAIL_PORT = 465
 
 MAIL_USE_SSL = True
 
-SITE_ADDRESS = 'http://sharethesunshine.com'
+SITE_ADDRESS = 'https://sharethesunshine.com'
