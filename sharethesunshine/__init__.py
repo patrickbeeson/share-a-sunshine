e064ee6efdc7adfbdc0db945b529ed1fb1bbf427
Patrick Beeson <patrickbeeson@gmail.com>
2014-06-17 14:06:43 -0400

    Add credentials to SMTPHandler for error emails.

--- sharethesunshine/__init__.py
+++ sharethesunshine/__init__.py
@@ -20,13 +20,17 @@ login_manager.init_app(app)
 
 ADMINS = app.config['ADMINS']
 MAIL_SERVER = app.config['MAIL_SERVER']
+MAIL_USERNAME = app.config['MAIL_USERNAME']
+MAIL_PASSWORD = app.config['MAIL_PASSWORD']
+credentials = (MAIL_USERNAME, MAIL_PASSWORD)
 if not app.debug:
     file_handler = RotatingFileHandler('sunshine_errors.log', maxBytes=10000, backupCount=1)
     file_handler.setLevel(logging.INFO)
     app.logger.addHandler(file_handler)
     mail_handler = SMTPHandler(MAIL_SERVER,
                                'error@share.drinkthesunshine.com',
-                               ADMINS, 'An error has occureds')
+                               ADMINS, 'An error has occureds',
+                               credentials=credentials)
     mail_handler.setLevel(logging.ERROR)
     app.logger.addHandler(mail_handler)
 
