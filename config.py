d9f3a43c197d3753ae5da8c024d82509a14d6795
Patrick Beeson <patrickbeeson@gmail.com>
2014-06-23 13:21:14 -0400

    Update config with live stripe keys, adjust gitignore file.

--- config.py
+++ config.py
@@ -1,10 +1,12 @@
 SECRET_KEY = '\x03V\x96\xb6{#\x9f\x14K\xef{\x86Z\x14\xafZK/-\x84\xb0O\xcd\x13'
 
 # Testing
-STRIPE_SECRET_KEY = 'sk_test_HsuAieq5wo8ZSAm3lEushARi'
-STRIPE_PUBLIC_KEY = 'pk_test_7MmO7bkJvlkV0TfD1YfFcnVf'
+#STRIPE_SECRET_KEY = 'sk_test_HsuAieq5wo8ZSAm3lEushARi'
+#STRIPE_PUBLIC_KEY = 'pk_test_7MmO7bkJvlkV0TfD1YfFcnVf'
 
 # Live
+STRIPE_SECRET_KEY = 'sk_live_n4OGJVSC81yBSXl7R0gdjkmF'
+STRIPE_PUBLIC_KEY = 'pk_live_XDeeIdG9LbgtgpPdEJdUwtV4'
 
 # Development
 #SQLALCHEMY_DATABASE_URI = 'sqlite:///sharethesunshine.db'
