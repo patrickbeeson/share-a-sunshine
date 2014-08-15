5a7f819dbf6aa63a762ecc36e6f8ae062b32fe29
Patrick Beeson <patrickbeeson@gmail.com>
2014-08-15 11:51:20 -0400

    Add new images, revisions to base and home templates and main SCSS.

--- config.py
+++ config.py
@@ -1,18 +1,18 @@
 SECRET_KEY = '\x03V\x96\xb6{#\x9f\x14K\xef{\x86Z\x14\xafZK/-\x84\xb0O\xcd\x13'
 
 # Testing
-#STRIPE_SECRET_KEY = 'sk_test_HsuAieq5wo8ZSAm3lEushARi'
-#STRIPE_PUBLIC_KEY = 'pk_test_7MmO7bkJvlkV0TfD1YfFcnVf'
+STRIPE_SECRET_KEY = 'sk_test_HsuAieq5wo8ZSAm3lEushARi'
+STRIPE_PUBLIC_KEY = 'pk_test_7MmO7bkJvlkV0TfD1YfFcnVf'
 
 # Live
-STRIPE_SECRET_KEY = 'sk_live_n4OGJVSC81yBSXl7R0gdjkmF'
-STRIPE_PUBLIC_KEY = 'pk_live_XDeeIdG9LbgtgpPdEJdUwtV4'
+#STRIPE_SECRET_KEY = 'sk_live_n4OGJVSC81yBSXl7R0gdjkmF'
+#STRIPE_PUBLIC_KEY = 'pk_live_XDeeIdG9LbgtgpPdEJdUwtV4'
 
 # Development
-#SQLALCHEMY_DATABASE_URI = 'sqlite:///sharethesunshine.db'
+SQLALCHEMY_DATABASE_URI = 'sqlite:///sharethesunshine.db'
 
 # Production
-SQLALCHEMY_DATABASE_URI = 'postgresql://sharethesunshine:ClearsTheClouds@localhost:5432/sharethesunshine'
+#SQLALCHEMY_DATABASE_URI = 'postgresql://sharethesunshine:ClearsTheClouds@localhost:5432/sharethesunshine'
 
 ADMINS = ['pbeeson@thevariable.com']
 
