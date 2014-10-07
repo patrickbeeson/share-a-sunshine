26dc3f5a128ca3053104d05af724b4951b635448
Patrick Beeson <patrickbeeson@gmail.com>
2014-10-07 17:47:31 -0400

    More styling for desktop and mobile.

--- sharethesunshine/static/gen/packed.js
+++ sharethesunshine/static/gen/packed.js
@@ -1,7 +1,7 @@
 
 
-$(document).ready(function(){$('.prefilled_message button').click(function(){var prefilled_message=$(this).parent().find('.message').text();var custom_message=$('#personal_message')
-$(custom_message).val(prefilled_message).blur();$(document).scrollTop($("#message").offset().top-100);});});(function($){$.fn.equalHeight=function(){var heights=[];$.each(this,function(i,element){$element=$(element);var element_height;var includePadding=($element.css('box-sizing')=='border-box')||($element.css('-moz-box-sizing')=='border-box');if(includePadding){element_height=$element.innerHeight();}else{element_height=$element.height();}
+$(document).ready(function(){$('.prefilled_message button').click(function(){ var prefilled_message=$(this).parent().find('.message').text(); var custom_message=$('#personal_message') 
+$(custom_message).val(prefilled_message).blur(); $(document).scrollTop($("#message").offset().top-100);});});(function($){$.fn.equalHeight=function(){var heights=[];$.each(this,function(i,element){$element=$(element);var element_height;var includePadding=($element.css('box-sizing')=='border-box')||($element.css('-moz-box-sizing')=='border-box');if(includePadding){element_height=$element.innerHeight();}else{element_height=$element.height();}
 heights.push(element_height);});this.height(Math.max.apply(window,heights));return this;};$.fn.equalHeightGrid=function(columns){var $tiles=this;$tiles.css('height','auto');for(var i=0;i<$tiles.length;i++){if(i%columns===0){var row=$($tiles[i]);for(var n=1;n<columns;n++){row=row.add($tiles[i+n]);}
 row.equalHeight();}}
 return this;};$.fn.detectGridColumns=function(){var offset=0,cols=0;this.each(function(i,elem){var elem_offset=$(elem).offset().top;if(offset===0||elem_offset==offset){cols++;offset=elem_offset;}else{return false;}});return cols;};$.fn.responsiveEqualHeightGrid=function(){var _this=this;function syncHeights(){var cols=_this.detectGridColumns();_this.equalHeightGrid(cols);}
--- sharethesunshine/static/scripts/main.js
+++ sharethesunshine/static/scripts/main.js
@@ -4,10 +4,15 @@
 // If prefilled message is selected, populate it in the textfield
 $( document ).ready(function() {
     $('.prefilled_message button').click(function() {
+        // Grab the prefilled message text
         var prefilled_message = $(this).parent().find('.message').text();
+        // Find our textarea
         var custom_message = $('#personal_message')
+
+        //Insert the prefilled message into the textarea
         $(custom_message).val(prefilled_message).blur();
 
+        // Send the user to the message area with some offset to account for the header
         $(document).scrollTop( $("#message").offset().top - 100 );
     });
 });
